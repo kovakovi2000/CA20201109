f6e971f3a63dfc3e5dcca820ac42c6c34ea607b5
Kovács Bence <kovacs.bence.kova@gmail.com>
2020-11-09 13:15:32 +0100

    ftrtt

--- CA20201109/Program.cs
+++ CA20201109/Program.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace CA20201109
 {
     class Program
     {
+        static SqlConnection conn;
         static void Main(string[] args)
         {
+            conn = new SqlConnection(
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                @"AttachDbFilename=|DataDirectory|\Res\teszt2.mdf;");
 
+            conn.Open();
 
+            //new SqlCommand("INSERT INTO emberek (orszagId, nev) VALUES (1, 'Zsofika');", conn).ExecuteNonQuery();
+
+            var sqlcmd = new SqlCommand("SELECT * FROM emberek;", conn);
+
+            var r = sqlcmd.ExecuteReader();
+            while (r.Read())
+                Console.WriteLine(r[2]);
+
+            conn.Close();
+
+            Console.ReadKey();
         }
     }
 }
